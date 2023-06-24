08f787d117ad36c2f7d227f077a2a6ad02ef1489
Deepthi Pauly <deepthipaul.c@gmail.com>
2023-06-24 19:14:33 +0530

    view todo created

--- src/components/todo/todo.jsx
+++ src/components/todo/todo.jsx
@@ -1,25 +1,51 @@
-import React from 'react'
-import { Button, Col, Container, Row } from 'react-bootstrap'
-import Card from 'react-bootstrap/Card';
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import { BACKEND_URL } from "../../constants/constant";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { selectUser } from "../../feature/auth/authSlice";
 
 function Todo() {
+  const [viewtodos, setViewTodos] = useState();
+  const userData = useSelector(selectUser);
+  console.log("user", userData);
+  const params = useParams();
+  const todo = async (todoId) => {
+    try {
+      const todoData = await axios.get(
+        `${BACKEND_URL}/task/view_todo/${todoId}`,
+        {
+          headers: {
+            access_token: userData.token,
+          },
+        }
+      );
+      setViewTodos(todoData.data.data);
+    } catch (e) {
+      console.log("error in fetch todos", e);
+      alert("unable to get todos");
+    }
+  };
+
+  useEffect(() => {
+    todo(params.id);
+  }, []);
+
   return (
     <Container>
-      <Row className="align-items-center" style={{ height: "100vh" }}>
+      <Row className="align-items-center" style={{ height: "85vh" }}>
         <Col className="my-auto">
           <div className="d-flex justify-content-center">
             <Card>
               <Card.Body>
                 <Card.Title>
-                  Special title treatment
+                  {viewtodos?.title}
                   <i class="fa-sharp fa-solid fa-file-pen fa-beat-fade ms-1"></i>{" "}
                 </Card.Title>
-                <Card.Text>
-                  With supporting text below as a natural lead-in to additional
-                  content.
-                </Card.Text>
-                <Card.Text>Last updated 3 mins ago</Card.Text>
+                <Card.Text>{viewtodos?.description}</Card.Text>
+                <Card.Text>{viewtodos?.updatedAt}</Card.Text>
               </Card.Body>
             </Card>
           </div>
--- src/components/todoList/todoList.jsx
+++ src/components/todoList/todoList.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
-import { Container,Row,Col, Button } from 'react-bootstrap'
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
+import { BACKEND_URL } from "../../constants/constant";
+import axios from "axios";
+import { selectUser } from "../../feature/auth/authSlice";
+import { useSelector } from "react-redux";
+import "./todolist.css";
 
 function TodoList() {
+  const [showAddTodo, setShowAddTodo] = useState(false)
+  const [todos, setTodos] = useState([]);
+  const userData = useSelector(selectUser);
+  console.log("userData",userData)
+  const fetchAllTodos = async () => {
+    try {
+      const allTodos = await axios.get(`${BACKEND_URL}/task/all_todo`,{
+      headers: {
+        access_token: userData.token
+      }
+      });
+      setTodos(allTodos.data.data);
+    } catch (e) {
+      console.log("error in fetch todos", e);
+      alert("unable to fetch todos");
+    }
+  };
+
+  const updateAddTodo = () => {
+    if (!userData.token) {
+      alert("Please Login to Add Todos");
+      setShowAddTodo(false);
+      return;
+    }
+    setShowAddTodo((prev) => !prev);
+  };
+
+  useEffect(() => {
+    fetchAllTodos();
+  }, []);
+
   return (
     <Container>
       <Row>
@@ -14,26 +49,28 @@ function TodoList() {
           <Button>
             <i className="fa-solid fa-plus fa-beat fa-sm"></i>
           </Button>
-          <Link to={"/todo/1"}>
-            <div className="mt-2">
-              <InputGroup className="mb-3">
-                <InputGroup.Text id="basic-addon1">
-                  <b>Status</b>
-                </InputGroup.Text>
-                <Form.Control
-                  placeholder={""}
-                  aria-label="Username"
-                  aria-describedby="basic-addon1"
-                />
-                <InputGroup.Text>
-                  <i className="fa-solid fa-trash fa-fade"></i>
-                </InputGroup.Text>
-              </InputGroup>
-            </div>
-          </Link>
+            {todos.map((allTodos) => (
+              <Link to={`/todo/${allTodos._id}`} className="statusStyle">
+              <div key={allTodos._id} className="mt-2">
+                <InputGroup className="mb-3">
+                  <InputGroup.Text id="basic-addon1">
+                    <b>{allTodos.status}</b>
+                  </InputGroup.Text>
+                    <Form.Control className="fs-3"
+                     placeholder={allTodos.title}
+                      aria-label="Username"
+                      aria-describedby="basic-addon1"
+                    />
+                  <InputGroup.Text>
+                    <i className="fa-solid fa-trash fa-fade"></i>
+                  </InputGroup.Text>
+                </InputGroup>
+              </div>
+              </Link>
+            ))}
         </Col>
       </Row>
     </Container>
   );
 }
-export default TodoList
+export default TodoList;
